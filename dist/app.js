67653e8545e3887e23ca118b71c36e7cb4884475
vahdet <vahdetkeskin@gmail.com>
2021-07-23 16:07:58 +0300

    add a dummy method

--- dist/app.js
+++ dist/app.js
@@ -1,11 +1,24 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.lambdaHandler = void 0;
+const getNewKits = () => {
+    return [];
+};
+const resolvers = {
+    Query: {
+        posts: (ctx) => {
+            return getNewKits();
+        }
+    }
+};
 const lambdaHandler = async (event) => {
-    const queries = JSON.stringify(event.queryStringParameters);
-    return {
-        statusCode: 200,
-        body: `Queries: ${queries}`
-    };
+    const typeHandler = resolvers[event.info.parentTypeName];
+    if (typeHandler) {
+        const resolver = typeHandler[event.info.fieldName];
+        if (resolver) {
+            return await resolver(event);
+        }
+    }
+    throw new Error(`Resolver not found for event: ${JSON.stringify(event)}`);
 };
 exports.lambdaHandler = lambdaHandler;
--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,27 @@
-import { AppSyncResolverEvent, ProxyResult } from 'aws-lambda'
+import { AppSyncResolverEvent } from 'aws-lambda'
 import { RDSClient, AddRoleToDBClusterCommand } from '@aws-sdk/client-rds'
+
+const getNewKits = () => {
+  return []
+}
+
+const resolvers = {
+  Query: {
+    posts: (ctx) => {
+      return getNewKits()
+    }
+  }
+}
 
 export const lambdaHandler = async (
   event: AppSyncResolverEvent<any>
-): Promise<ProxyResult> => {
-  const queries = JSON.stringify(event.queryStringParameters)
-
-  switch (event.field) {
-    case 'getPost':
-      var id = event.arguments.id
-      return {
-        statusCode: 200,
-        body: `Queries: ${queries}`
-      }
-      break
-    default:
-      break
+): Promise<any> => {
+  const typeHandler = resolvers[event.info.parentTypeName]
+  if (typeHandler) {
+    const resolver = typeHandler[event.info.fieldName]
+    if (resolver) {
+      return await resolver(event)
+    }
   }
+  throw new Error(`Resolver not found for event: ${JSON.stringify(event)}`)
 }
